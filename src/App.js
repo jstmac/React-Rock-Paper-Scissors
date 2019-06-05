da38144b21cd1e86762ec8065e50ac8fea29fc5d
Melissa G <jstmac@gmail.com>
2019-06-05 16:09:57 -0600

    Choices Working

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import scissors from "./Images/scissors.jpeg";
 import { Container, Header, Button } from "semantic-ui-react";
 
 class App extends React.Component {
-  state = { userChoice: null, compChoice: null };
+  state = { userChoice: null , compChoice: null };
 
-  optionClick = () => {
-    return
+  optionArr= ["Rock", "Paper", "Scissors"];
 
-
+  optionClick = (choice) => {
+    //this.state.userChoice
+    const { userChoice } = this.state
+    this.setState({userChoice: choice})
   };
+  
+  randomCompChoice = () => {
+   return this.optionArr[Math.floor(Math.random() * this.optionArr.length)]
+  }
 
+  computerResult = () => {
+    return this.state.userChoice !== null ? <div>{this.randomCompChoice()}</div> : <div></div>
+  }
+  
   render() {
     return (
       <Container style={{ marginTop: "25px" }}>
@@ -21,11 +31,20 @@ class App extends React.Component {
         <br />
         <Header as="h2">Player please make a selection</Header>
         <hr />
-        <OptionBox name="rock" img={rock} />
-        <OptionBox name="paper" img={paper} />
-        <OptionBox name="scissors" img={scissors} />      
+        <OptionBox name="rock" img={rock} optionClick={this.optionClick}/>
+        <OptionBox name="paper" img={paper} optionClick={this.optionClick}/>
+        <OptionBox name="scissors" img={scissors} optionClick={this.optionClick}/>   
+        <div>your choice: {this.state.userChoice}</div>
+        <div>computer choice: {this.computerResult()}</div>   
       </Container>
+
+      
+
+      
     );
+
+
+    
   }
 }
 
