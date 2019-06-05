ca01075712147fd77d36f6b75a2bba5d7c15777b
Melissa G <jstmac@gmail.com>
2019-06-05 15:21:18 -0600

    Pics working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import OptionBox from "./OptionBox";
+import rock from "./Images/rock.jpg";
+import paper from "./Images/paper.jpeg";
+import scissors from "./Images/scissors.jpeg";
+import { Container, Header, Button } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { userChoice: null, compChoice: null };
+
+  optionClick = () => {
+    return
+
+
+  };
+
+  render() {
+    return (
+      <Container style={{ marginTop: "25px" }}>
+        <Header as="h1">Rock|Paper|Scissors</Header>
+        <br />
+        <Header as="h2">Player please make a selection</Header>
+        <hr />
+        <OptionBox name="rock" img={rock} />
+        <OptionBox name="paper" img={paper} />
+        <OptionBox name="scissors" img={scissors} />      
+      </Container>
+    );
+  }
 }
 
 export default App;
